028b3133eb16f177fa231ba63ab45da5a12daa3e
Gavin Wolfe <gavinwolfe@Gavins-MacBook-Air.local>
2017-12-02 17:13:51 -0800

    Initial Commit

---
+++ Likeful/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Likeful
+//
+//  Created by Gavin Wolfe on 12/2/17.
+//  Copyright © 2017 Gavin Wolfe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
